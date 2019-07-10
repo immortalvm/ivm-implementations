a41fc9bfa33c30d8017d5eb3aee4be6c524d99e7
Ivar Rummelhoff <Ivar.Rummelhoff@nr.no>
2019-07-10 20:01:36 +0200

    Assembler essentially done (!)

--- Assembler/Composition.fs
+++ Assembler/Composition.fs
@@ -265,175 +265,6 @@ let pop n =
 let changeSign = [NOT; PUSH1; 1y; ADD]
 let isZero = [PUSH1; 1y; LESS_THAN]
 
-open Assembler.Ast
-
-let byteDist x = -128 <= x && x <= 127
-
-// NB. The delta is w.r.t. before the code.
-let deltaJump delta =
-    if delta = 0 then []
-    elif byteDist <| delta - 3 then [PUSH0; JUMP_ZERO; int8 (delta - 3)]
-    elif delta < 0 then [GET_PC] @ pushInt (delta - 1) @ [ADD; JUMP]
-    else 
-        let f pushLength = delta - pushLength - 1
-        pushFix f @ [GET_PC; ADD; JUMP]
-
-let deltaJumpZero delta =
-    if byteDist <| delta - 2 then [JUMP_ZERO; int8 (delta - 2)]
-    else
-        let jump = deltaJump (delta - 5)
-        [JUMP_ZERO; 3y; PUSH0; JUMP_ZERO; int8 (opLen jump)] @ jump
-
-let deltaJumpNotZero delta = isZero @ deltaJumpZero (delta - opLen isZero)
-
-let genericConditional interjection =
-    [
-        GET_STACK; PUSH1; 8y; ADD; LOAD8 // a::x::r -> x::a::x::r
-    ] @ interjection @ [
-        JUMP_ZERO; 6y
-        // If not zero:
-        GET_STACK; PUSH1; 8y; ADD; STORE8 // a::x::r -> a::r
-        JUMP
-    ] @ pop 2 // If zero
-
-let genericJumpNotZero = genericConditional []
-let genericJumpZero = genericConditional isZero
-
-// TODO: There should be a built-in function like this.
-let valueOr<'a> (def: 'a) (x: 'a option) = match x with Some z -> z | _ -> def
-let mapGet<'a, 'b when 'a: comparison> (m: Map<'a,'b>) (x: 'a) (def: 'b) =
-    m.TryFind x |> valueOr def
-
-type Spes = int8 list * int64
-
-let collapseSpes ((code, acc): Spes) : int8 list =
-    match code, acc with
-    | _, 0L -> code
-    | [PUSH0], _ -> pushNum acc
-    | _, _ -> code @ pushNum acc @ [ADD]
-
-let zeroSpes: Spes = [PUSH0], 0L
-let oneSpes: Spes = [PUSH0], 1L
-
-let addSpes ((c1, a1): Spes) ((c2, a2): Spes) =
-    let c = match c1, c2 with
-            | _, [PUSH0] -> c1
-            | [PUSH0], _ -> c2
-            | _, _ -> c1 @ c2 @ [ADD]
-    (c, a1 + a2)
-
-let minusSpes ((code, acc): Spes) : Spes =
-    match code with
-    | [PUSH0] -> code, -acc
-    | _ -> code @ changeSign, -acc
-
-let multSpes (s1: Spes) (s2: Spes) : Spes =
-    match s1, s2 with
-    | ([PUSH0], m), ([PUSH0], n) -> ([PUSH0], m * n)
-    | _, ([PUSH0], 0L) -> zeroSpes
-    | ([PUSH0], 0L), _ -> zeroSpes
-    | _, ([PUSH0], 1L) -> s1
-    | ([PUSH0], 1L), _ -> s2
-    | (c, m), ([PUSH0], n) -> c @ pushNum n @ [MULTIPLY], m * n
-    | ([PUSH0], m), (c, n) -> c @ pushNum m @ [MULTIPLY], m * n
-    | _, _ -> collapseSpes s1 @ collapseSpes s2 @ [MULTIPLY], 0L
-
-let timesNSpes (n: int64) (s: Spes) : Spes = multSpes s ([PUSH0], n)
-
-let rec exprPushCore
-        (expression: Expression)
-        (lookup: int -> int)
-        (position: int)
-        (depth: int) : Spes =
-    let mutable spes = zeroSpes
-    let inner e =
-        let (p, d) = match spes with
-                     | [PUSH0], _ -> position, depth
-                     | code, _ -> position + opLen code, depth + 1
-        exprPushCore e lookup p d
-
-    match expression with
-    | ENum n -> spes <- [PUSH0], n
-    | ELabel i -> spes <- [GET_PC], int64 (lookup i - position - 1)
-
-    | EStack e ->
-        spes <- [GET_STACK], 0L
-        let s = inner e |> timesNSpes 8L
-        spes <- addSpes spes s
-
-    | ESum lst ->
-        let f e =
-            match e with
-            | ELabel _ -> 1 | EMinus (ELabel _) -> -1
-            | EStack _ -> 2 | EMinus (EStack _) -> -2
-            | _ -> 0
-        let cnt = new Map<int,int>(List.countBy f lst)
-        let nPc = mapGet cnt 1 0 - mapGet cnt -1 0
-        let nSp = mapGet cnt 2 0 - mapGet cnt -2 0
-
-        spes <- addSpes spes <| timesNSpes (int64 nPc) ([GET_PC], 0L)
-        spes <- addSpes spes <| timesNSpes (int64 nSp) ([GET_STACK], 0L)
-
-        for ex in lst do
-            let s = match ex with
-                    | ELabel i -> [PUSH0], int64 (lookup i - position)
-                    | EMinus (ELabel i) -> [PUSH0], int64 (position - lookup i)
-                    | EStack e -> inner e |> timesNSpes 8L
-                    | EMinus (EStack e) -> inner e |> timesNSpes -8L
-                    | e -> inner e
-            spes <- addSpes spes s
-
-    | EProd lst ->
-        spes <- oneSpes
-        for e in lst do spes <- multSpes spes (inner e)
-
-    // TODO
-    | _ -> ()
-    spes
-
-
-let expressionPush e lookup = exprPushCore e lookup 0 0 |> collapseSpes
-
-let expressionAdd e lookup =
-    match exprPushCore e lookup 0 0 with
-    | [PUSH0], 0L -> []
-    | spes -> collapseSpes spes @ [ADD]
-
-let expressionMult e lookup =
-    match exprPushCore e lookup 0 0 with
-    | [PUSH0], 1L -> []
-    | [PUSH0], 0L -> pop 1 @ [PUSH0] // Pop value, push 0.
-    | spes -> collapseSpes spes @ [MULTIPLY]
-
-let expressionAnd e lookup =
-    match exprPushCore e lookup 0 0 with
-    | [PUSH0], -1L -> []
-    | [PUSH0], 0L -> pop 1 @ [PUSH0] // Pop value, push 0.
-    | spes -> collapseSpes spes @ [AND]
-
-let expressionOr e lookup =
-    match exprPushCore e lookup 0 0 with
-    | [PUSH0], 0L -> []
-    | [PUSH0], -1L -> pop 1 @ [PUSH0; NOT] // Pop value, push -1.
-    | spes -> collapseSpes spes @ [OR]
-
-let expressionXor e lookup =
-    match exprPushCore e lookup 0 0 with
-    | [PUSH0], 0L -> []
-    | [PUSH0], -1L -> [NOT]
-    | spes -> collapseSpes spes @ [XOR]
-
-
-let expressionDivU e lookup =
-    match exprPushCore e lookup 0 0 with
-    | [PUSH0], 1L -> []
-    | spes -> collapseSpes spes @ [DIVIDE]
-
-let expressionRemU e lookup =
-    match exprPushCore e lookup 0 0 with
-    | [PUSH0], 1L -> pop 1 @ [PUSH0] // Pop value, push 0.
-    | spes -> collapseSpes spes @ [REMAINDER]
-
 module Helpers =
     let addr n = [GET_STACK; PUSH1; int8 (n * 8); ADD]
     let get n = addr n @ [LOAD8]
@@ -491,6 +322,344 @@ module Helpers =
     let gtS = offsetSign2 @ gtU
     let gteS = offsetSign2 @ gteU
 
+open Assembler.Ast
+
+let byteDist x = -128 <= x && x <= 127
+
+// NB. The delta is w.r.t. before the code.
+let deltaJump delta =
+    if delta = 0 then []
+    elif byteDist <| delta - 3 then [PUSH0; JUMP_ZERO; int8 (delta - 3)]
+    elif delta < 0 then [GET_PC] @ pushInt (delta - 1) @ [ADD; JUMP]
+    else 
+        let f pushLength = delta - pushLength - 1
+        pushFix f @ [GET_PC; ADD; JUMP]
+
+let deltaJumpZero delta =
+    if byteDist <| delta - 2 then [JUMP_ZERO; int8 (delta - 2)]
+    else
+        let jump = deltaJump (delta - 5)
+        [JUMP_ZERO; 3y; PUSH0; JUMP_ZERO; int8 (opLen jump)] @ jump
+
+let deltaJumpNotZero delta = isZero @ deltaJumpZero (delta - opLen isZero)
+
+let genericConditional interjection =
+    [
+        GET_STACK; PUSH1; 8y; ADD; LOAD8 // a::x::r -> x::a::x::r
+    ] @ interjection @ [
+        JUMP_ZERO; 6y
+        // If not zero:
+        GET_STACK; PUSH1; 8y; ADD; STORE8 // a::x::r -> a::r
+        JUMP
+    ] @ pop 2 // If zero
+
+let genericJumpNotZero = genericConditional []
+let genericJumpZero = genericConditional isZero
+
+// TODO: There should be a built-in function like this.
+let valueOr<'a> (def: 'a) (x: 'a option) = match x with Some z -> z | _ -> def
+let mapGet<'a, 'b when 'a: comparison> (m: Map<'a,'b>) (x: 'a) (def: 'b) =
+    m.TryFind x |> valueOr def
+
+type Spes = int8 list * int64
+
+let collapseSpes ((code, acc): Spes) : int8 list =
+    match code, acc with
+    | _, 0L -> code
+    | [PUSH0], _ -> pushNum acc
+    | _, _ -> code @ pushNum acc @ [ADD]
+
+let collapseSpes2 (s1: Spes) (s2: Spes) : int8 list =
+    match s1 with
+    // NB: In this case s2 expects to go first, see 'inner' in exprPushCore!
+    | [PUSH0], _ -> collapseSpes s2 @ collapseSpes s1
+    | _ -> collapseSpes s1 @ collapseSpes s2
+
+let zeroSpes: Spes = [PUSH0], 0L
+let oneSpes: Spes = [PUSH0], 1L
+let trueSpes: Spes = [PUSH0], -1L
+
+let addSpes ((c1, a1): Spes) ((c2, a2): Spes) =
+    let c = match c1, c2 with
+            | _, [PUSH0] -> c1
+            | [PUSH0], _ -> c2
+            | _, _ -> c1 @ c2 @ [ADD]
+    (c, a1 + a2)
+
+let minusSpes ((code, acc): Spes) : Spes =
+    match code with
+    | [PUSH0] -> [PUSH0], -acc
+    | _ -> code @ changeSign, -acc
+
+let multSpes (s1: Spes) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> ([PUSH0], m * n)
+    | _, ([PUSH0], 0L) -> zeroSpes
+    | ([PUSH0], 0L), _ -> zeroSpes
+    | _, ([PUSH0], 1L) -> s1
+    | ([PUSH0], 1L), _ -> s2
+    | (c, m), ([PUSH0], n) -> c @ pushNum n @ [MULTIPLY], m * n
+    | ([PUSH0], m), (c, n) -> c @ pushNum m @ [MULTIPLY], m * n
+    | _, _ -> collapseSpes2 s1 s2 @ [MULTIPLY], 0L
+
+let timesNSpes (n: int64) (s: Spes) : Spes = multSpes s ([PUSH0], n)
+
+let pow2Spes (spes: Spes) : Spes =
+    match spes with
+    | [PUSH0], n -> [PUSH0], if n < 0L || n > 63L then 0L else 1L <<< int n
+    | _ -> collapseSpes spes @ [POW2], 0L
+
+let andSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], m &&& n
+    | _, ([PUSH0], 0L) -> ([PUSH0], 0L)
+    | ([PUSH0], 0L), _ -> ([PUSH0], 0L)
+    | _, ([PUSH0], -1L) -> s1
+    | ([PUSH0], -1L), _ -> s2
+    | _ -> collapseSpes2 s1 s2 @ [AND], 0L
+
+let orSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], m ||| n
+    | _ -> collapseSpes2 s1 s2 @ [OR], 0L
+
+let xorSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], m ^^^ n
+    | _ -> collapseSpes2 s1 s2 @ [XOR], 0L
+
+let negSpes ((code, acc): Spes) : Spes =
+    match code with
+    | [PUSH0] -> [PUSH0], acc ^^^ -1L
+    | _ -> code @ [NOT], acc ^^^ -1L
+
+let sign1Spes (spes: Spes) : Spes =
+    match spes with
+    | [PUSH0], n -> [PUSH0], n |> uint8 |> int8 |> int64
+    | _ -> collapseSpes spes @ [SIGN1], 0L
+
+let sign2Spes (spes: Spes) : Spes =
+    match spes with
+    | [PUSH0], n -> [PUSH0], n |> uint16 |> int16 |> int64
+    | _ -> collapseSpes spes @ [SIGN2], 0L
+
+let sign4Spes (spes: Spes) : Spes =
+    match spes with
+    | [PUSH0], n -> [PUSH0], n |> uint32 |> int32 |> int64
+    | _ -> collapseSpes spes @ [SIGN4], 0L
+
+
+let divUSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    // Do not eliminate division by zero:
+    | _, ([PUSH0], 0L) -> [PUSH0; PUSH0; DIVIDE], 0L
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], (uint64 m / uint64 n |> int64)
+    | _, ([PUSH0], 1L) -> s1
+    | ([PUSH0], 0L), _ -> zeroSpes
+    | _, _ -> collapseSpes2 s1 s2 @ [DIVIDE], 0L
+
+let divSSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    // Do not eliminate division by zero:
+    | _, ([PUSH0], 0L) -> [PUSH0; PUSH0; DIVIDE], 0L
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], m / n
+    | _, ([PUSH0], 1L) -> s1
+    | ([PUSH0], 0L), _ -> zeroSpes
+    | (c, m), ([PUSH0], -1L) -> c @ changeSign, -m
+    | _, _ -> collapseSpes2 s1 s2 @ Helpers.divS, 0L
+
+let remUSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    // Do not eliminate division by zero:
+    | _, ([PUSH0], 0L) -> [PUSH0; PUSH0; REMAINDER], 0L
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], (uint64 m % uint64 n |> int64)
+    | _, ([PUSH0], 1L) -> zeroSpes
+    | ([PUSH0], 0L), _ -> zeroSpes
+    | _, _ -> collapseSpes2 s1 s2 @ [REMAINDER], 0L
+
+let remSSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    // Do not eliminate division by zero:
+    | _, ([PUSH0], 0L) -> [PUSH0; PUSH0; REMAINDER], 0L
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], m % n
+    | _, ([PUSH0], 1L) -> zeroSpes
+    | ([PUSH0], 0L), _ -> zeroSpes
+    | _, ([PUSH0], -1L) -> zeroSpes
+    | _, _ -> collapseSpes2 s1 s2 @ Helpers.remS, 0L
+
+let eqSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], if m = n then -1L else 0L
+    | _, _ -> collapseSpes2 s1 s2 @ Helpers.eq, 0L
+
+let ltUSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], if uint64 m < uint64 n then -1L else 0L
+    | _, _ -> collapseSpes2 s1 s2 @ Helpers.ltU, 0L
+
+let ltSSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], if m < n then -1L else 0L
+    | _, _ -> collapseSpes2 s1 s2 @ Helpers.ltS, 0L
+
+let lteUSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], if uint64 m <= uint64 n then -1L else 0L
+    | _, _ -> collapseSpes2 s1 s2 @ Helpers.lteU, 0L
+
+let lteSSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], if m <= n then -1L else 0L
+    | _, _ -> collapseSpes2 s1 s2 @ Helpers.lteS, 0L
+
+let gtUSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], if uint64 m > uint64 n then -1L else 0L
+    | _, _ -> collapseSpes2 s1 s2 @ Helpers.gtU, 0L
+
+let gtSSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], if m > n then -1L else 0L
+    | _, _ -> collapseSpes2 s1 s2 @ Helpers.gtS, 0L
+
+let gteUSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], if uint64 m >= uint64 n then -1L else 0L
+    | _, _ -> collapseSpes2 s1 s2 @ Helpers.gteU, 0L
+
+let gteSSpes (s1: Spes ) (s2: Spes) : Spes =
+    match s1, s2 with
+    | ([PUSH0], m), ([PUSH0], n) -> [PUSH0], if m >= n then -1L else 0L
+    | _, _ -> collapseSpes2 s1 s2 @ Helpers.gteS, 0L
+
+
+let rec exprPushCore
+        (expression: Expression)
+        (lookup: int -> int)
+        (position: int)
+        (depth: int) : Spes =
+    let mutable spes = zeroSpes
+
+    let inner e =
+        let (p, d) = match spes with
+                     | [PUSH0], _ -> position, depth
+                     | code, _ -> position + opLen code, depth + 1
+        exprPushCore e lookup p d
+
+    let process1 f e = spes <- f (inner e)
+    let process2 f e1 e2 = spes <- inner e1; spes <- f spes (inner e2)
+    let processList f s lst =
+        spes <- s
+        for e in lst do spes <- f spes (inner e)
+
+    match expression with
+    | ENum n -> spes <- [PUSH0], n
+    | ELabel i -> spes <- [GET_PC], int64 (lookup i - position - 1)
+
+    | EStack e ->
+        spes <- [GET_STACK], 0L
+        let s = inner e |> timesNSpes 8L
+        spes <- addSpes spes s
+
+    | ESum lst ->
+        let f e =
+            match e with
+            | ELabel _ -> 1 | EMinus (ELabel _) -> -1
+            | EStack _ -> 2 | EMinus (EStack _) -> -2
+            | _ -> 0
+        let cnt = new Map<int,int>(List.countBy f lst)
+        let nPc = mapGet cnt 1 0 - mapGet cnt -1 0
+        let nSp = mapGet cnt 2 0 - mapGet cnt -2 0
+
+        spes <- addSpes spes <| timesNSpes (int64 nPc) ([GET_PC], 0L)
+        spes <- addSpes spes <| timesNSpes (int64 nSp) ([GET_STACK], 0L)
+
+        for ex in lst do
+            let s = match ex with
+                    | ELabel i -> [PUSH0], int64 (lookup i - position)
+                    | EMinus (ELabel i) -> [PUSH0], int64 (position - lookup i)
+                    | EStack e -> inner e |> timesNSpes 8L
+                    | EMinus (EStack e) -> inner e |> timesNSpes -8L
+                    | e -> inner e
+            spes <- addSpes spes s
+
+    | EProd lst -> processList multSpes oneSpes lst
+    | EMinus e -> process1 minusSpes e
+    | EPow2 e -> spes <- pow2Spes <| inner e
+    | EConj lst -> processList andSpes trueSpes lst
+    | EDisj lst -> processList orSpes zeroSpes lst
+    | EXor lst -> processList xorSpes zeroSpes lst
+    | ENeg e -> process1 negSpes e
+    | ESign1 e -> process1 sign1Spes e
+    | ESign2 e -> process1 sign2Spes e
+    | ESign4 e -> process1 sign4Spes e
+
+    | EDivU (e1, e2) -> process2 divUSpes e1 e2
+    | EDivS (e1, e2) -> process2 divSSpes e1 e2
+    | ERemU (e1, e2) -> process2 remUSpes e1 e2
+    | ERemS (e1, e2) -> process2 remSSpes e1 e2
+
+    | EEq (e1, e2) -> process2 eqSpes e1 e2
+    | ELtU (e1, e2) -> process2 ltUSpes e1 e2
+    | ELtS (e1, e2) -> process2 ltSSpes e1 e2
+    | ELtEU (e1, e2) -> process2 lteUSpes e1 e2
+    | ELtES (e1, e2) -> process2 lteSSpes e1 e2
+    | EGtU (e1, e2) -> process2 gtUSpes e1 e2
+    | EGtS (e1, e2) -> process2 gtSSpes e1 e2
+    | EGtEU (e1, e2) -> process2 gteUSpes e1 e2
+    | EGtES (e1, e2) -> process2 gteSSpes e1 e2
+
+    | ELoad1 e -> spes <- collapseSpes (inner e) @ [LOAD1], 0L
+    | ELoad2 e -> spes <- collapseSpes (inner e) @ [LOAD2], 0L
+    | ELoad4 e -> spes <- collapseSpes (inner e) @ [LOAD4], 0L
+    | ELoad8 e -> spes <- collapseSpes (inner e) @ [LOAD8], 0L
+
+    // Finally we are able to return the result:
+    spes
+
+
+let expressionPush e lookup = exprPushCore e lookup 0 0 |> collapseSpes
+
+let expressionAdd e lookup =
+    match exprPushCore e lookup 0 0 with
+    | [PUSH0], 0L -> []
+    | spes -> collapseSpes spes @ [ADD]
+
+let expressionMult e lookup =
+    match exprPushCore e lookup 0 0 with
+    | [PUSH0], 1L -> []
+    | [PUSH0], 0L -> pop 1 @ [PUSH0] // Pop value, push 0.
+    | spes -> collapseSpes spes @ [MULTIPLY]
+
+let expressionAnd e lookup =
+    match exprPushCore e lookup 0 0 with
+    | [PUSH0], -1L -> []
+    | [PUSH0], 0L -> pop 1 @ [PUSH0] // Pop value, push 0.
+    | spes -> collapseSpes spes @ [AND]
+
+let expressionOr e lookup =
+    match exprPushCore e lookup 0 0 with
+    | [PUSH0], 0L -> []
+    | [PUSH0], -1L -> pop 1 @ [PUSH0; NOT] // Pop value, push -1.
+    | spes -> collapseSpes spes @ [OR]
+
+let expressionXor e lookup =
+    match exprPushCore e lookup 0 0 with
+    | [PUSH0], 0L -> []
+    | [PUSH0], -1L -> [NOT]
+    | spes -> collapseSpes spes @ [XOR]
+
+
+let expressionDivU e lookup =
+    match exprPushCore e lookup 0 0 with
+    | [PUSH0], 1L -> []
+    | spes -> collapseSpes spes @ [DIVIDE]
+
+let expressionRemU e lookup =
+    match exprPushCore e lookup 0 0 with
+    | [PUSH0], 1L -> pop 1 @ [PUSH0] // Pop value, push 0.
+    | spes -> collapseSpes spes @ [REMAINDER]
+
 let expressionDivS e lookup =
     match exprPushCore e lookup 0 0 with
     | [PUSH0], 1L -> []
@@ -511,7 +680,6 @@ let intermediates (prog: Statement list) : seq<Intermediate> =
     let frag r code = fragment r <| fun _ -> code
     let withDelta i r f = rest <- r; [Fragment <| fun lookup -> f (lookup i)]
 
-    // We avoid recursion, just in case.
     seq {
         while not rest.IsEmpty do
         yield!
@@ -585,5 +753,5 @@ let intermediates (prog: Statement list) : seq<Intermediate> =
 
             | SPush e :: r -> fragment r (expressionPush e)
 
-            | _ -> failwithf "Unhandled statement: %O" rest.[0]
+            | [] -> failwith "Impossible case"
     }
