e09d65f99c12f48d573e7291ebfef9964dad5ff9
Ivar Rummelhoff <Ivar.Rummelhoff@nr.no>
2019-07-12 14:54:47 +0200

    Specify RuntimeFrameworkVersion for all the assemblies published
    This is done to avoid conflicts with Visual Studio. There might be better
    alternatives.

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 dotnet clean
 
 # See https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-publish .
-# Not sure if '-f netcoreapp2.1' is useful since this is also in the project files.
+# I'm not sure if '-f netcoreapp2.1' is useful since this is also in the project files.
 dotnet publish Command -c release --self-contained -f netcoreapp2.1 -r osx-x64
 dotnet publish Command -c release --self-contained -f netcoreapp2.1 -r win-x64
 dotnet publish Command -c release --self-contained -f netcoreapp2.1 -r linux-x64
