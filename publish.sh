812622bc54c321fb293d4da9ff50136a644d663c
Ivar Rummelhoff <Ivar.Rummelhoff@nr.no>
2019-08-18 16:16:44 +0200

    Improve publish.sh

--- publish.sh
+++ publish.sh
@@ -3,10 +3,21 @@
 # See https://stackoverflow.com/a/3355423
 cd "$(dirname "$0")"
 
+TAG=$(git describe --tags --abbrev=0)
+echo "Publishing $TAG"
+
 dotnet clean
 
 # See https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-publish .
 # I'm not sure if '-f netcoreapp2.1' is useful since this is also in the project files.
-dotnet publish Command -c release --self-contained -f netcoreapp2.1 -r osx-x64
-dotnet publish Command -c release --self-contained -f netcoreapp2.1 -r win-x64
-dotnet publish Command -c release --self-contained -f netcoreapp2.1 -r linux-x64
+
+ pub() {
+    dotnet publish Command -c release --self-contained -f netcoreapp2.1 -r $1
+    pushd Command/bin/release/netcoreapp2.1/$1
+    zip -r "$TAG"_$1.zip publish
+    popd
+}
+
+pub osx-x64
+pub linux-x64
+pub win-x64
