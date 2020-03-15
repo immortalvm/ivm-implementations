9018f45e93c8131880d49fd5db5207c8c3d6db2b
Ivar Rummelhoff <ivarrru@gmail.com>
2020-03-16 00:17:17 +0100

    Update C implementation of the VM
    It has been tested with Clang (cc -Ofast vm.c). For now, the I/O
    instructions all have dummy implementations except put_char.

--- OtherMachines/vm.c
+++ OtherMachines/vm.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <wchar.h>
+#include <unistd.h>
 
 #if UINTPTR_MAX != 0xffffffffffffffff
 #error The address space must be 64-bit.
@@ -12,7 +13,7 @@
 #error The byte ordering must be little-endian.
 #endif
 
-#define WRONG_NUMBER_OF_ARGUMENTS 1
+#define OPTION_PARSE_ERROR 1
 #define FILE_NOT_FOUND 2
 #define NOT_IMPLEMENTED_YET 3
 #define UNDEFINED_INSTRUCTION 4
@@ -40,8 +41,6 @@
 #define STORE2 21
 #define STORE4 22
 #define STORE8 23
-#define ALLOCATE 24
-#define DEALLOCATE 25
 #define ADD 32
 #define MULT 33
 #define DIV 34
@@ -52,30 +51,80 @@
 #define NOT 42
 #define XOR 43
 #define POW2 44
-#define NEW_FRAME 48
-#define SET_PIXEL 49
-#define ADD_SAMPLE 50
-#define PUT_CHAR 52
-#define READ_FRAME 56
-#define READ_PIXEL 57
+
+#define READ_FRAME (uint8_t)-1
+#define READ_PIXEL (uint8_t)-2
+#define NEW_FRAME (uint8_t)-3
+#define SET_PIXEL (uint8_t)-4
+#define ADD_SAMPLE (uint8_t)-5
+#define PUT_CHAR (uint8_t)-6
 
 void* pc;
 void* sp;
 
-void push(uint64_t x) { *((uint64_t*) (sp -= 8)) = x; }
-uint64_t pop() { uint64_t result = *((uint64_t*) sp); sp += 8; return result; }
+static inline void push(uint64_t x) { *((uint64_t*) (sp -= 8)) = x; }
+static inline uint64_t pop() { uint64_t result = *((uint64_t*) sp); sp += 8; return result; }
 
-uint8_t next1() { return *((uint8_t*)(pc++)); }
-uint16_t next2() { uint16_t result = *((uint16_t*)pc); pc += 2; return result; }
-uint32_t next4() { uint32_t result = *((uint32_t*)pc); pc += 4; return result; }
-uint64_t next8() { uint64_t result = *((uint64_t*)pc); pc += 8; return result; }
+static inline uint8_t next1() { return *((uint8_t*)(pc++)); }
+static inline uint16_t next2() { uint16_t result = *((uint16_t*)pc); pc += 2; return result; }
+static inline uint32_t next4() { uint32_t result = *((uint32_t*)pc); pc += 4; return result; }
+static inline uint64_t next8() { uint64_t result = *((uint64_t*)pc); pc += 8; return result; }
 
-uint64_t signExtend1(uint64_t x) { return ((uint64_t)(int64_t)(int8_t)(uint8_t)x); }
-uint64_t signExtend2(uint64_t x) { return ((uint64_t)(int64_t)(int16_t)(uint16_t)x); }
-uint64_t signExtend4(uint64_t x) { return ((uint64_t)(int64_t)(int32_t)(uint32_t)x); }
+static inline uint64_t signExtend1(uint64_t x) { return ((uint64_t)(int64_t)(int8_t)(uint8_t)x); }
+static inline uint64_t signExtend2(uint64_t x) { return ((uint64_t)(int64_t)(int16_t)(uint16_t)x); }
+static inline uint64_t signExtend4(uint64_t x) { return ((uint64_t)(int64_t)(int32_t)(uint32_t)x); }
+
+uint64_t memorySize = 1 << 24;
+char* argFile = NULL;
+char* inpDir = NULL;
+char* outDir = NULL;
+char* binFile = NULL;
+
+// cf. https://pubs.opengroup.org/onlinepubs/7908799/xsh/getopt.html
+void parseOptions(int argc, char** argv) {
+  int c;
+  int errflg = 0;
+  extern char* optarg;
+  extern int optind, optopt;
+
+  while ((c = getopt(argc, argv, ":m:a:i:o:")) != -1) {
+    switch (c) {
+    case 'm':
+      memorySize = strtoull(optarg, (char**)NULL, 10);
+      break;
+    case 'a':
+      argFile = optarg;
+      break;
+    case 'i':
+      inpDir = optarg;
+      break;
+    case 'o':
+      outDir = optarg;
+      break;
+    case ':':
+      fprintf(stderr, "Option -%c requires an operand\n", optopt);
+      errflg++;
+      break;
+    case '?':
+      fprintf(stderr, "Unrecognised option: -%c\n", optopt);
+      errflg++;
+    }
+  }
+
+  if (optind != argc - 1) {
+    fprintf(stderr, "A single binary file must be specified.\n");
+    errflg++;
+  } else {
+    binFile = argv[optind++];
+  }
+  if (errflg) {
+    fprintf(stderr, "Usage: %s [-m <bytes>] [-a <file>] [-o <dir>] binary\n", argv[0]);
+    exit(OPTION_PARSE_ERROR);
+  }
+}
 
 /* Based on https://stackoverflow.com/a/22059317. */
-int readFile(char* filename, int additonalZeros, void** start) {
+long readFile(char* filename, void* start) {
   FILE* fileptr = fopen(filename, "rb");
   if (fileptr == NULL) {
     fprintf(stderr, "File not found or not readable: %s\n", filename);
@@ -85,27 +134,29 @@ int readFile(char* filename, int additonalZeros, void** start) {
   long filelen = ftell(fileptr);
   rewind(fileptr);
 
-  int size = filelen + additonalZeros;
-  *start = calloc(size, 1);
-  fread(*start, filelen, 1, fileptr);
+  fread(start, filelen, 1, fileptr);
   fclose(fileptr);
-  return size;
+  return filelen;
 }
 
 /* Example: clang vm.c && ./a.out 50 ex3_quick_sort.bin empty */
+// [-m <bytes>] [-a <file>] [-o <dir>] binary
 int main(int argc, char** argv) {
-  if (argc != 4) {
-    fprintf(stderr, "Usage:\n  %s <stack elements> <binary file> <argument file>\n", argv[0]);
-    return WRONG_NUMBER_OF_ARGUMENTS;
-  }
+  parseOptions(argc, argv);
 
-  int stackElements = (int) strtol(argv[1], (char**)NULL, 10);
-  readFile(argv[2], 0, &pc);
-  int argBytes = readFile(argv[3], 3*8, &sp);
-  sp += argBytes;
+  void* memStart = calloc(memorySize, 1);
+  void* memStop = memStart + memorySize;
 
+  void* argStart = memStart + readFile(binFile, memStart) + 8;
+  long argLength = argFile == NULL ? 0 : readFile(argFile, argStart);
+  *((uint64_t*) (argStart - 8)) = argLength;
+  void* heapStart = argStart + argLength;
+
+  pc = memStart;
+  sp = memStop;
   bool terminated = false;
   uint64_t x, y;
+  bool undefIo = false;
 
   while (!terminated) {
     switch (next1()) {
@@ -144,9 +195,6 @@ int main(int argc, char** argv) {
     case STORE4: x = pop(); *((uint32_t*)x) = (uint32_t) pop(); break;
     case STORE8: x = pop(); *((uint64_t*)x) = pop(); break;
 
-    case ALLOCATE: push((uint64_t) calloc(pop(), 1)); break;
-    case DEALLOCATE: free((void*) pop()); break;
-
     case ADD: push(pop() + pop()); break;
     case MULT: push(pop() * pop()); break;
     case DIV:
@@ -169,21 +217,22 @@ int main(int argc, char** argv) {
     case POW2: x = pop(); push(x <= 63 ? 1UL << x : 0); break;
 
     // TODO:
-    case NEW_FRAME:
-    case SET_PIXEL:
-    case ADD_SAMPLE:
-      fprintf(stderr, "Not implemented yet\n");
-      return NOT_IMPLEMENTED_YET;
+    case READ_FRAME: undefIo = true; push(0); push(0); break;
+    case READ_PIXEL: undefIo = true; pop(); pop(); push(0); break;
+    case NEW_FRAME: undefIo = true; printf("\r\f"); pop(); pop(); pop(); break;
+    case SET_PIXEL: undefIo = true; pop(); pop(); pop(); pop(); pop(); break;
+    case ADD_SAMPLE: undefIo = true; pop(); pop(); break;
 
     case PUT_CHAR: printf("%lc", (wchar_t)pop()); break;
     default:
-      fprintf(stderr, "Undefined instruction\n");
+      fprintf(stderr, "Undefined instruction: %d\n", *((uint8_t*)(pc-1)));
       return UNDEFINED_INSTRUCTION;
     }
   }
 
   // Print stack
-  while (stackElements-- > 0) {
+  printf("\n");
+  while (sp != memStop) {
     x = pop();
     printf("0x..%05llX %7lld\n", x & 0xfffffUL, x);
   }
