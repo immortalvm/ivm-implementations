6dcdc815219f018aecbc577daeedc50cd9042b1d
Ivar Rummelhoff <ivarrru@gmail.com>
2020-03-23 10:01:55 +0100

    Improve example 6

--- Assembler.Tests/test_code/ex6_clone_frames.s
+++ Assembler.Tests/test_code/ex6_clone_frames.s
@@ -1,9 +1,7 @@
 ### Write (B/W versions of) each input frame to the output
-###
-### NB. As of March 2020, read_frame does not work correctly on all platforms.
 
     set_sp! &-3
-per_frame:
+next_frame:                      # Possible entry point
     read_frame
     x = 4
     y = 3
@@ -17,11 +15,10 @@ per_frame:
 next_row:
     store8!! 0 &x
 next_col:
-    ## This would be more elegant if we allow read_pixel in expressions.
+    ## This would be more elegant if we allowed read_pixel in expressions.
     read_pixel* [$x $y]
     store8! &(+ v 1)
 
-    put_char! 48
     set_pixel* [$x $y $v $v $v]
     store8!! (+ $x 1) &x
     jump_not_zero!! (<u $x $w) next_col
@@ -30,7 +27,7 @@ next_col:
     jump_not_zero!! (<u $y $h) next_row
 
     set_sp! &2                  # Pop w,h
-    jump! per_frame
+    jump! next_frame
 
 done:
     set_sp! &5                  # Pop x,y,v,w,h
