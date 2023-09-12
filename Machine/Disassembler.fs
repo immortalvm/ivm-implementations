92172e051da562699d6b5765b89c59e9e9ae4385
Ivar Rummelhoff <ivarru@nr.no>
2023-09-12 12:10:18 +0200

    Fix disassembler bug
    The bug was introduced when adding VERSION to Literal.fs.

--- Machine/Disassembler.fs
+++ Machine/Disassembler.fs
@@ -4,11 +4,11 @@ open System.Reflection
 
 let instructionNames : string[] =
     let t = typeof<Machine.Instructions.IReflectionMarker>.DeclaringType
-    let toPair (field: FieldInfo) : string * sbyte =
-        field.Name, downcast field.GetValue None
-    let pairs = t.GetFields(BindingFlags.Public ||| BindingFlags.Static)
-                |> Seq.map toPair
     let result = Array.create 256 "-"
-    for (key, value) in pairs do
-        result.[value |> uint8 |> int] <- key
+    for (key, value) in
+            t.GetFields(BindingFlags.Public ||| BindingFlags.Static)
+            |> Seq.map (fun field -> field.Name, field.GetValue None)
+            |> Seq.where (fun (_, value) -> value :? sbyte)
+            |> Seq.map (fun (name, value) -> name, value :?> sbyte |> uint8 |> int) do
+        result.[value] <- key
     result
